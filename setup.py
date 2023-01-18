096deb67fd87e85b71b5c09c3b6ad86ff52fd69d
Benjamin W. Portner <benjaminwportner@gmail.com>
2023-01-18 02:53:10 +0100

    add email to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Benjamin W. Portner",
-    author_email="",
+    author_email="benwportner@gmail.com",
     url="https://github.com/BenPortner/pydbk",
     packages=["pydbk"],
     install_requires=[],
