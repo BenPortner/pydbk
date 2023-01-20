2a89473e943481533b82e50500255e65674080df
Benjamin W. Portner <benjaminwportner@gmail.com>
2023-01-20 02:55:44 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pydbk",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python tool to extract .dbk archives.",
     long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type="text/markdown",
